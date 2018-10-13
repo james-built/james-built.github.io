7fb6e0fdaa54d9c9fa6df69d9c568602be1ffabf
james-built <jgbuilt@gmail.com>
2018-10-14 12:00:09 +1300

    copied font across to react layout, added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=''>
+      <a href="https://www.instagram.com/james.b.snapping/"><img src="../icons/instagram50.png"/></a>
+      <a href="https://twitter.com/gunslingerNZ"><img src="../icons/twitter50.png"/></a>
+      <a href="https://github.com/james-built"><img src="../icons/github50.png"/></a>
+    </div>
+  )
+}
+
+export default Footer
